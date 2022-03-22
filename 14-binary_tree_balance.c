1324067e64df4e1bb747d4885caa414d5bb5e7bc
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-03-22 09:49:52 -0300

    Task 14. Betty NOT tested

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer 2 the root node of the tree 2 traverse.
+ * Return: int -> height of a binary tree.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
