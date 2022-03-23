7b113a81fdc00bfb5649859c5f5340dbe0140149
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-03-23 09:37:50 -0300

    Task 101. Some proto and main

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+/**
+ *print_level - Print the nodes by level.
+ *@func: Pointer 2 a function.
+ *@level: Level
+ *@node: Pointer 2 the node.
+ *Return: Nada
+ */
+
+ void print_level(binary_tree_t *node, void (*func)(int), int level)
+
+ /**
+ *binary_tree_levelorder - Level-order traversal.
+ *@tree: Pointer 2 the tree.
+ *@func: Pointer 2 a function.
+ *Return: Nada
+ */
+
+ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
