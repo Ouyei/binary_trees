249f16e93b7b3b24c819902af81b17f9e412e6a8
RayBar72 <3963@holbertonschool.com>
2022-03-23 16:02:12 -0300

    Try cero

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = calloc(1, sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 	{
@@ -20,7 +20,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node->parent = parent;
 	new_node->n = value;
-	new_node->left = new_node->right = NULL;
 
 	return (new_node);
 }
