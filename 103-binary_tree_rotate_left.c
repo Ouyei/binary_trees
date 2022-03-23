4a93698b8bb2f1ab4d53edd6c7271b8d99f162a0
Ouyei <obedat@.com>
2022-03-23 07:30:41 -0700

    Task 103. Betty checked

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -7,3 +7,20 @@
  */
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *tmp, *aux;
+
+	tmp = NULL;
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
+	if (tree->right->left != NULL)
+		tmp = tree->right->left;
+	aux = tree->right;
+	tree->right->left = tree;
+	tree->parent = tree->right;
+	tree->right->parent = NULL;
+	tree->right = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	return (aux);
+}
